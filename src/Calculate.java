06fee44e42790d2e48fc1162b1ad7aba45b8715a
suxaryk <suxaryk@ukr.net>
2015-05-04 00:01:17 +0200

    + junit xml ant

---
+++ src/Calculate.java
@@ -0,0 +1,5 @@
+public class Calculate {
+    public int calA(int a, int b){
+        return a+b;
+    }
+}
---
+++ testFolder/codec/CalculateTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+public class CalculateTest {
+    @Test
+    public void testCalA() throws Exception {
+        Calculate calculate = new Calculate();
+        int n = calculate.calA(2, 2);
+
+        assertEquals(4, n);
+    }
+}
